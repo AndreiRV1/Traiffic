bdcfeead9881888ad6d581a041906b4eb8270c87
StefBisti <stefbisti@gmail.com>
2025-11-09 11:49:39 +0200

    Added road assets, simple road rendering and got rid of road ID
    Signed-off-by: StefBisti <stefbisti@gmail.com>

--- main.py
+++ main.py
@@ -4,19 +4,17 @@ from core import settings
 
 
 def main():
-    # Initialize pygame
     pygame.init()
     pygame.display.set_caption(settings.WINDOW_TITLE)
 
-    # Create the screen
+    # create screen
     screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
-    # Create and run the game loop
+    # create game
     game = GameLoop(screen, clock)
     game.run()
 
-    # Quit pygame cleanly
     pygame.quit()
 
 
